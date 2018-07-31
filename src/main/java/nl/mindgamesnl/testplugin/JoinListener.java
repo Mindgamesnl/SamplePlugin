27cc0d14476dac830d248dee3bbb205fd23adc7b
Mindgamesnl <matsmoolhuizen@gmail.com>
2018-07-31 07:00:10 +0200

    Add files via upload

---
+++ src/main/java/nl/mindgamesnl/testplugin/JoinListener.java
@@ -0,0 +1,15 @@
+package nl.mindgamesnl.testplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinListener implements Listener {
+
+@EventHandler
+public void onJoin(PlayerJoinEvent event) {
+    String name = event.getPlayer().getName();
+    event.setJoinMessage("Er is een nieuwe speler bij gekomen! wat een prutser die " + name);
+}
+
+}
---
+++ src/main/java/nl/mindgamesnl/testplugin/TestPlugin.java
@@ -0,0 +1,17 @@
+package nl.mindgamesnl.testplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestPlugin extends JavaPlugin {
+
+@Override
+public void onEnable() {
+    // Plugin startup logic
+    getServer().getPluginManager().registerEvents(new JoinListener(), this);
+}
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
